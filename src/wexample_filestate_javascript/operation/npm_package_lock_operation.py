808a050bf8aa94e2ea9c32f0d2fb62a3e58a550d
weeger <romain.weeger@gmail.com>
2026-03-30 17:50:48 +0200

    Publishing version 0.0.15

--- src/wexample_filestate_javascript/operation/npm_package_lock_operation.py
+++ src/wexample_filestate_javascript/operation/npm_package_lock_operation.py
@@ -3,10 +3,9 @@ from __future__ import annotations
 from pathlib import Path
 from typing import TYPE_CHECKING
 
+from wexample_filestate.operation.abstract_operation import AbstractOperation
 from wexample_helpers.decorator.base_class import base_class
 from wexample_helpers.helpers.shell import shell_run
-
-from wexample_filestate.operation.abstract_operation import AbstractOperation
 
 if TYPE_CHECKING:
     from wexample_filestate.enum.scopes import Scope
