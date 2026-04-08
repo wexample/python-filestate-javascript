8f15c52479ce6e9614b181d9c0f5f99642ccd7ce
weeger <romain.weeger@gmail.com>
2026-04-08 22:42:06 +0200

    Scopes everywhere

--- src/wexample_filestate_javascript/option/javascript/npm_package_lock_option.py
+++ src/wexample_filestate_javascript/option/javascript/npm_package_lock_option.py
@@ -4,17 +4,21 @@ from pathlib import Path
 from typing import TYPE_CHECKING, Any
 
 from wexample_config.config_option.abstract_config_option import AbstractConfigOption
+from wexample_filestate.enum.scopes import Scope
 from wexample_filestate.option.mixin.option_mixin import OptionMixin
 from wexample_helpers.decorator.base_class import base_class
 
 if TYPE_CHECKING:
     from wexample_filestate.const.types_state_items import TargetFileOrDirectoryType
-    from wexample_filestate.enum.scopes import Scope
     from wexample_filestate.operation.abstract_operation import AbstractOperation
 
 
 @base_class
 class NpmPackageLockOption(OptionMixin, AbstractConfigOption):
+    @classmethod
+    def get_scopes(cls) -> list[Scope]:
+        return [Scope.CONTENT]
+
     @staticmethod
     def get_raw_value_allowed_type() -> Any:
         return bool
--- src/wexample_filestate_javascript/option/javascript_option.py
+++ src/wexample_filestate_javascript/option/javascript_option.py
@@ -20,6 +20,10 @@ if TYPE_CHECKING:
 
 @base_class
 class JavascriptOption(OptionMixin, WithRunnerOptionMixin, AbstractNestedConfigOption):
+    @classmethod
+    def get_scopes(cls) -> list[Scope]:
+        return [Scope.CONTENT]
+
     @staticmethod
     def get_raw_value_allowed_type() -> Any:
         from wexample_filestate_javascript.config_value.javascript_config_value import (
