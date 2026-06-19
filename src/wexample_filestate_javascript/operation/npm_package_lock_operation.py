00d2c74ddd3fb1b11e399c8eb7e56c689b33b1b4
weeger <romain.weeger@gmail.com>
2026-06-19 21:25:21 +0200

    Refactor

--- src/wexample_filestate_javascript/operation/npm_package_lock_operation.py
+++ src/wexample_filestate_javascript/operation/npm_package_lock_operation.py
@@ -5,7 +5,7 @@ from typing import TYPE_CHECKING
 
 from wexample_filestate.operation.abstract_operation import AbstractOperation
 from wexample_helpers.decorator.base_class import base_class
-from wexample_helpers.helpers.shell import shell_run
+from wexample_helpers.helper.shell import shell_run
 
 if TYPE_CHECKING:
     from wexample_filestate.enum.scopes import Scope
